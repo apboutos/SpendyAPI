5b2854f6b67e919fac24a15e3cff536cc59de8c9
Apostolos Boutos <apboutos@gmail.com>
2024-08-01 14:06:59 +0300

    Initial commit

---
+++ src/main/java/com/apboutos/spendy/spendyapi/SpendyApiApplication.java
@@ -0,0 +1,13 @@
+package com.apboutos.spendy.spendyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpendyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apboutos/spendy/spendyapi/SpendyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apboutos.spendy.spendyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
